5bf8b76e3ec93c13a185a9ed5cd87f65a0ae9783
Leslie Myint <leslie.myint@gmail.com>
2017-09-17 12:15:37 -0400

    Add guide lines, increase sim size, set seed, remove p1 option

--- common_tests/app.R
+++ common_tests/app.R
@@ -30,7 +30,7 @@ ui <- navbarPage(
                     value = 0,
                     step = 0.1
                 ),
-                actionButton(inputId = "repeat_cont", label = "Repeat 500 times")
+                actionButton(inputId = "repeat_cont", label = "Repeat 1000 times")
             ),
             mainPanel(
                 plotOutput(outputId = "plot_reject_cont")
@@ -44,28 +44,20 @@ ui <- navbarPage(
                 sliderInput(
                     inputId = "sample_size_categ",
                     label = "Total table sample size",
-                    min = 10,
-                    max = 300,
-                    value = 10,
-                    step = 10
-                ),
-                sliderInput(
-                    inputId = "prop_group1",
-                    label = "True proportion of cases in group 1",
-                    min = 0,
-                    max = 1,
-                    value = 0.1,
-                    step = 0.01
+                    min = 20,
+                    max = 1000,
+                    value = 20,
+                    step = 40
                 ),
                 sliderInput(
                     inputId = "effect_size_categ",
                     label = "True difference in proportions",
                     min = 0,
-                    max = 1,
+                    max = 0.4,
                     value = 0,
-                    step = 0.01
+                    step = 0.05
                 ),
-                actionButton(inputId = "repeat_categ", label = "Repeat 500 times")
+                actionButton(inputId = "repeat_categ", label = "Repeat 1000 times")
             ),
             mainPanel(
                 plotOutput(outputId = "plot_reject_categ")
@@ -76,10 +68,11 @@ ui <- navbarPage(
 
 server <- function(input, output) {
     observeEvent(input$repeat_cont, {
+        set.seed(1)
         if (input$distribution=="sym") {
             mean1 <- 10
             sdev <- 2
-            pvals <- replicate(500, {
+            pvals <- replicate(1000, {
                 mean2 <- mean1 + input$effect_size_cont
                 dat <- data.frame(group1 = rnorm(input$sample_size_cont, mean1, sdev), group2 = rnorm(input$sample_size_cont, mean2, sdev))
                 ttest_pval <- t.test(dat$group1, dat$group2)$p.value
@@ -89,7 +82,7 @@ server <- function(input, output) {
         } else {
             mu1 <- 0
             sdev <- 1
-            pvals <- replicate(500, {
+            pvals <- replicate(1000, {
                 mean2 <- exp(mu1 + (sdev^2)/2) + input$effect_size_cont
                 mu2 <- log(mean2) - (sdev^2)/2
                 dat <- data.frame(group1 = rlnorm(input$sample_size_cont, mu1, sdev), group2 = rlnorm(input$sample_size_cont, mu2, sdev))
@@ -104,15 +97,16 @@ server <- function(input, output) {
         output$plot_reject_cont <- renderPlot({
             plot(alpha_seq, ecdf_ttest, pch = 16, xlim = range(alpha_seq), ylim = range(ecdf_ttest, ecdf_wilcox), xlab = "alpha (Nominal type I error rate)", ylab = "Proportion of p-values less than alpha")
             points(alpha_seq, ecdf_wilcox, pch = 16, col = "red")
+            abline(a = 0, b = 1, col = "black", lty = "dashed")
             legend("topleft", legend = c("t-test", "Wilcoxon"), fill = c("black", "red"), bty = "n")
         })
     })
     observeEvent(input$repeat_categ, {
-        prop1 <- input$prop_group1
+        prop1 <- 0.1
+        prop2 <- min(prop1 + input$effect_size_categ, 1)
         n1 <- floor(input$sample_size_categ/2)
         n2 <- input$sample_size_categ - n1
-        pvals <- replicate(500, {
-            prop2 <- prop1 + input$effect_size_categ
+        pvals <- replicate(1000, {
             repeat({
                 group1 <- rbinom(n1, size = 1, prob = prop1)
                 group2 <- rbinom(n2, size = 1, prob = prop2)
@@ -131,6 +125,7 @@ server <- function(input, output) {
         output$plot_reject_categ <- renderPlot({
             plot(alpha_seq, ecdf_chi, pch = 16, xlim = range(alpha_seq), ylim = range(ecdf_chi, ecdf_fet), xlab = "alpha (Nominal type I error rate)", ylab = "Proportion of p-values less than alpha")
             points(alpha_seq, ecdf_fet, pch = 16, col = "red")
+            abline(a = 0, b = 1, col = "black", lty = "dashed")
             legend("topleft", legend = c("Chi-squared", "Fisher's"), fill = c("black", "red"), bty = "n")
         })
     })
